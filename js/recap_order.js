a691ca85ff85cf63ace18274661ccf62dd8502aa
Xamilu <maximeprioleau@gmail.com>
2021-02-11 21:12:34 +0100

    total price recap + css animations and commentaries

--- js/recap_order.js
+++ js/recap_order.js
@@ -1,12 +1,19 @@
+// Recover items which is in the cart
 let cart = JSON.parse(localStorage.getItem('cart'));
 
 let tbody = $('#finalcart');
 
+let total = 0;
+
+// For each round of loop we add the price of each item to the total price and display items informations
 for (let i = 0; i < cart.length; i ++){
+    
+    total += parseFloat(cart[i].price)
     
     let tr = '<tr><td><img src="'+ cart[i].image +'" style = "width: 100px"></td><td>'+ cart[i].title +'</td><td>'+ cart[i].price +'</td><td>1</td></tr>'
     tbody.append(tr);
-    
-    
 }
 
+// Display the total price
+let totalPrice = '<h2> Prix de la Commande: '+ total +'€</h2>'
+tbody.append(totalPrice);
